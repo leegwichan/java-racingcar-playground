df6ccbdbf9e642122bf82252e94d47918fdb1583
leegwichan <cnddkscndgus@naver.com>
2023-07-26 20:15:40 +0900

    feat (StringAddCalculator) : ',' 또는 ':' 문자를 기준으로 분리하여 숫자를 더하도록 구현

--- src/main/java/stringcalculator/StringAddCalculator.java
+++ src/main/java/stringcalculator/StringAddCalculator.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class StringAddCalculator {
 
     private static final int DEFAULT = 0;
-    private static final String SEPARATOR = ",";
+    private static final String SEPARATOR = ",|:";
 
     private StringAddCalculator() {
     }
