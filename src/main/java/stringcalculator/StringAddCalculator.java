7a978868c8e2e5c9d8ee20c89fd68b741425677a
leegwichan <cnddkscndgus@naver.com>
2023-07-26 20:55:45 +0900

    feat (StringAddCalculator) : custom seperator를 입력받을 수 있도록 변경

--- src/main/java/stringcalculator/StringAddCalculator.java
+++ src/main/java/stringcalculator/StringAddCalculator.java
@@ -1,11 +1,15 @@
 package stringcalculator;
 
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringAddCalculator {
 
     private static final int DEFAULT = 0;
-    private static final String SEPARATOR = ",|:";
+    private static final Pattern CUSTOM_SEPARATOR_PATTERN = Pattern.compile("//(.)\n(.*)");
+    private static final String DEFAULT_SEPARATOR_REGEX = "[,:]";
+    private static final String CUSTOM_SEPARATOR_REGEX_FORMAT = "[,:]|%s";
 
     private StringAddCalculator() {
     }
@@ -14,12 +18,25 @@ public class StringAddCalculator {
         if (isBlank(expression)) {
             return DEFAULT;
         }
-        return Arrays.stream(expression.split(SEPARATOR))
-                .mapToInt(Integer::parseInt)
-                .sum();
+
+        Matcher customDelimiterMatcher = CUSTOM_SEPARATOR_PATTERN.matcher(expression);
+        if (customDelimiterMatcher.find()) {
+            return calculate(customDelimiterMatcher);
+        }
+        return calculate(expression, DEFAULT_SEPARATOR_REGEX);
     }
 
     private static boolean isBlank(String expression) {
         return expression == null || expression.isBlank();
+    }
+
+    private static int calculate(Matcher customDelimiterMatcher) {
+        String splitRegex = String.format(CUSTOM_SEPARATOR_REGEX_FORMAT, customDelimiterMatcher.group(1));
+        String numberExpression = customDelimiterMatcher.group(2);
+        return calculate(numberExpression, splitRegex);
+    }
+
+    private static int calculate(String numberExpression, String splitRegex) {
+        return Arrays.stream(numberExpression.split(splitRegex)).mapToInt(Integer::parseInt).sum();
     }
 }
