7ac9b324175e76adf277778c88decc89023c4191
leegwichan <cnddkscndgus@naver.com>
2023-07-27 17:26:33 +0900

    feat (Car) : 랜덤 값에 따라 움직이는 기능 구현
    - 0~9까지 랜덤 값 중 4 이상인 경우 1칸 움직인다.

--- src/main/java/carrace/domain/Car.java
+++ src/main/java/carrace/domain/Car.java
@@ -1,10 +1,16 @@
 package carrace.domain;
 
+import java.util.Random;
+
 public final class Car {
 
     private static final int NAME_LENGTH_LIMIT = 5;
+    private static final int MAX_RANDOM = 9;
+    private static final int MOVING_MINIMUM = 4;
 
+    private final Random random = new Random();
     private final String name;
+    private int location = 0;
 
     private Car(String name) {
         if (name == null || name.isBlank()) {
@@ -18,5 +24,15 @@ public final class Car {
 
     public static Car of(String name) {
         return new Car(name);
+    }
+
+    public void move() {
+        if (random.nextInt(MAX_RANDOM) >= MOVING_MINIMUM) {
+            location++;
+        }
+    }
+
+    public int getLocation() {
+        return location;
     }
 }
--- src/test/java/carrace/domain/CarTest.java
+++ src/test/java/carrace/domain/CarTest.java
@@ -1,14 +1,22 @@
 package carrace.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.mockConstruction;
+import static org.mockito.Mockito.when;
 
+import java.util.Random;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.mockito.MockedConstruction;
 
 class CarTest {
+
+    private static final String DEFAULT_NAME = "steve";
 
     @DisplayName("적절한 조건을 통해 객체를 생성할 수 있다")
     @Nested
@@ -38,5 +46,24 @@ class CarTest {
         void creationTest(String name) {
             assertThatCode(() -> Car.of(name)).doesNotThrowAnyException();
         }
+    }
+
+    @DisplayName("차들은 규칙에 따라 움직일 수 있다")
+    @ParameterizedTest(name = "랜덤 값으로 {0}을 받으면 {1}칸 움직인다")
+    @CsvSource({"0,0", "3,0", "4,1", "9,1"})
+    void moveTest(int randomNumber, int expected) {
+        try (MockedConstruction<Random> mock = mockConstruction(Random.class)) {
+            Car car = Car.of(DEFAULT_NAME);
+            mockingRandom(mock, randomNumber);
+
+            car.move();
+
+            assertThat(car.getLocation()).isEqualTo(expected);
+        }
+    }
+
+    void mockingRandom(MockedConstruction<Random> mock, int stubNumber) {
+        Random mockRandom = mock.constructed().get(0);
+        when(mockRandom.nextInt(anyInt())).thenReturn(stubNumber);
     }
 }
